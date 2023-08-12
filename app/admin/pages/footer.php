6feb43fc07047063f23f7a396857784138ea1bc3
sandri1997 <sandriadnin26@gmail.com>
2023-08-12 21:09:45 +0700

    first commit

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.1
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?>. SANDRI ADNIN</strong> 
+</footer>
